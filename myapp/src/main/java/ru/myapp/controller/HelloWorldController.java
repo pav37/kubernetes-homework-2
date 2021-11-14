1d8c242ee4e8b5acf1d8f62d6e113784fbe7e52f
Andrey <sptp358@gmail.com>
2021-11-14 21:19:19 +0500

    Initial commit

---
+++ myapp/src/main/java/ru/myapp/controller/HelloWorldController.java
@@ -0,0 +1,30 @@
+package ru.myapp.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ru.myapp.model.Greeting;
+
+@Controller
+public class HelloWorldController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/hello")
+	@ResponseBody
+	public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@GetMapping("/hello/{studentName}")
+	@ResponseBody
+	public Greeting sayHelloStudent(@PathVariable() String studentName) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, studentName));
+	}
+
+}
